59f3bbb20e3e4d873de4a474fe72344604532319
Maria Danielson <mariama@Marias-MacBook-Air-2.local>
2019-10-14 16:26:13 -0400

    final

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 
   searchBooks = (term) => {
     this.setState({searchTerm: term, loading:true, error:null})  
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&filter=ebooks`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}`)
       .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
       .then(books => this.setState({
         books: books.items,
@@ -30,22 +30,42 @@ class App extends React.Component {
   }
 
   bookTypeFilter = (searchTerm, bookType, printType) => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&filter=${bookType}&printType=${printType}`)
-      .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
-      .then(books => this.setState({
-        books: books.items,
-      loading:false}))
-      .catch(error => this.setState({error, loading:false}));
+    // if (printType === 'magazines') {
+    //   return this.setState({error : 'invalid search', loading: false});
+    // }
+
+   
+      if(printType === 'magazines') {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&printType=${printType}`)
+        .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
+        .then(books => this.setState({
+          books: books.items,
+        loading:false}))
+        .catch(error => this.setState({error, loading:false}));
+      }
+      else {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&filter=${bookType}&printType=${printType}`)
+        .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
+        .then(books => this.setState({
+          books: books.items,
+        loading:false}))
+        .catch(error => this.setState({error, loading:false}));
+      }
+
+
+
 
   }
 
 
   render() {
+
+    let content = <BookList currency={USCurrencyFormat} books={this.state.books}/>
     if(this.state.error) {
-      return <div>Error: {this.state.error}</div>;
+      content= <div>Error: {this.state.error}</div>;
     }
     else if (this.state.loading) {
-      return <div>loading...</div>
+      content= <div>loading...</div>
     }
   return (
     <div className="App">
@@ -53,7 +73,7 @@ class App extends React.Component {
       <BookSearch searchBooks={this.searchBooks}/>
       <BookFilter searchTerm={this.state.searchTerm} bookTypeFilter={this.bookTypeFilter} />
       <div>
-        <BookList currency={USCurrencyFormat} books={this.state.books}/>
+        {content}
       </div>
     </div>
   );
