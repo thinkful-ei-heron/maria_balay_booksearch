0e68e067ac858c03e3985de7d87dc2030de19fe5
Maria Danielson <mariama@Marias-MacBook-Air-2.local>
2019-10-14 15:24:43 -0400

    complete app

--- src/App.js
+++ src/App.js
@@ -12,21 +12,46 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 
 class App extends React.Component {
   state = {
-      books: []
+      books: [],
+      searchTerm: '',
+      loading: false,
+      error: null
     }
 
-  searchBooks = (searchTerm) => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&filter=ebooks`)
-      .then(res => res.json())
-      .then(books => this.setState({books: books.items}))
+  searchBooks = (term) => {
+    this.setState({searchTerm: term, loading:true, error:null})  
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&filter=ebooks`)
+      .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
+      .then(books => this.setState({
+        books: books.items,
+      loading:false}))
+      .catch(error => this.setState({error, loading:false}));
+
   }
 
+  bookTypeFilter = (searchTerm, bookType, printType) => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&filter=${bookType}&printType=${printType}`)
+      .then(res => res.ok ? res.json() : Promise.reject('Something went wrong'))
+      .then(books => this.setState({
+        books: books.items,
+      loading:false}))
+      .catch(error => this.setState({error, loading:false}));
+
+  }
+
+
   render() {
+    if(this.state.error) {
+      return <div>Error: {this.state.error}</div>;
+    }
+    else if (this.state.loading) {
+      return <div>loading...</div>
+    }
   return (
     <div className="App">
       <Header />
       <BookSearch searchBooks={this.searchBooks}/>
-      <BookFilter />
+      <BookFilter searchTerm={this.state.searchTerm} bookTypeFilter={this.bookTypeFilter} />
       <div>
         <BookList currency={USCurrencyFormat} books={this.state.books}/>
       </div>
--- src/components/BookFilter.js
+++ src/components/BookFilter.js
@@ -2,22 +2,29 @@ import React from 'react'
 import './BookFilter.css'
 
 class BookFilter extends React.Component {
+    bookFilterComp = (e) => {
+        e.preventDefault()
+        const bookType = e.currentTarget.book_type.value
+        const printType = e.currentTarget.print_type.value
+        const searchTerm = this.props.searchTerm
+        this.props.bookTypeFilter(searchTerm, bookType, printType)
+    }
     render() {
         return (
-            <div className="book-filter">
+            <form className="book-filter" onChange={this.bookFilterComp}>
                 <label htmlFor="print-type">Print Type:</label>
-                <select className="print-type" name="print-type">
+                <select className="print_type" id="print_type">
                     <option value="all">All</option>
-                    <option value="book">Books</option>
+                    <option value="books">Books</option>
                     <option value="magazines">Magazines</option>
                 </select>
                 <label htmlFor="book-type">Book Type:</label>
-                <select className="book-type" name="book-type">
+                <select className="book-type" id="book_type">
                     <option value="ebooks">All</option>
                     <option value="free-ebooks">Free E-Books</option>
                     <option value="paid-ebooks">E-Books for purchase</option>
                 </select>
-            </div>
+            </form>
         )
     }
 }
